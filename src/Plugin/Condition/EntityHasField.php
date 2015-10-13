c2d7921a25f5b754e15530a1c93dfe3f77513781
Erin McLaughlin <emclaughlin@genuineinteractive.com>
2015-10-13 15:30:27 -0400

    Issue #2572119: Updating the type hint and type.

--- src/Plugin/Condition/EntityHasField.php
+++ src/Plugin/Condition/EntityHasField.php
@@ -7,7 +7,7 @@
 
 namespace Drupal\rules\Plugin\Condition;
 
-use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\rules\Core\RulesConditionBase;
 
 /**
@@ -36,7 +36,7 @@ class EntityHasField extends RulesConditionBase {
   /**
    * Checks if a given entity has a given field.
    *
-   * @param \Drupal\Core\Entity\EntityInterface $entity
+   * @param \Drupal\Core\Entity\FieldableEntityInterface $entity
    *   The entity to check for the provided field
    * @param string $field
    *   The field to check for on the entity
@@ -44,7 +44,7 @@ class EntityHasField extends RulesConditionBase {
    * @return bool
    *   TRUE if the provided entity has the provided field.
    */
-  protected function doEvaluate(EntityInterface $entity, $field) {
+  protected function doEvaluate(FieldableEntityInterface $entity, $field) {
     return $entity->hasField($field);
   }
 
